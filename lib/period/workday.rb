cd1190205459571d6488ca49a38e04c66852e405
Ruan Brandão <ruan.bernardo@gmail.com>
2018-07-05 14:08:10 -0300

    Add Period namespace to Workday module

--- lib/period/workday.rb
+++ lib/period/workday.rb
@@ -3,42 +3,44 @@ require 'active_support/core_ext'
 require 'set'
 require 'yaml'
 
-module Workday
-  # Hash of Arrays containing Dates of holidays
-  # Brasil  => 2001 - 2078
-  # Bovespa => 1993 - 2022
-  # TODO: add holidays before 2001 for :brasil (use :bovespa to infer?)
-  # TODO: remove weekend dates from :brasil
+class Period
+  module Workday
+    # Hash of Arrays containing Dates of holidays
+    # Brasil  => 2001 - 2078
+    # Bovespa => 1993 - 2022
+    # TODO: add holidays before 2001 for :brasil (use :bovespa to infer?)
+    # TODO: remove weekend dates from :brasil
 
-  CALENDARS = %i(brasil bovespa)
+    CALENDARS = %i(brasil bovespa)
 
-  HOLIDAYS = CALENDARS.map do |calendar|
-    holidays_file = File.open(File.join(File.dirname(__FILE__), "config/#{calendar}.yml"))
-    holidays = YAML.safe_load(holidays_file.read, [Date])
-    [calendar, Set.new(holidays[calendar.to_s])]
-  end.to_h
+    HOLIDAYS = CALENDARS.map do |calendar|
+      holidays_file = File.open(File.join(File.dirname(__FILE__), "config/#{calendar}.yml"))
+      holidays = YAML.safe_load(holidays_file.read, [Date])
+      [calendar, Set.new(holidays[calendar.to_s])]
+    end.to_h
 
-  def self.restday?(date, calendar: :brasil)
-    weekday = date.wday
+    def self.restday?(date, calendar: :brasil)
+      weekday = date.wday
 
-    weekday == 0 || #saturday
-    weekday == 6 || #sunday
-    HOLIDAYS[calendar].include?(date.to_date)
-  end
+      weekday == 0 || #saturday
+      weekday == 6 || #sunday
+      HOLIDAYS[calendar].include?(date.to_date)
+    end
 
-  def self.workday?(date, calendar: :brasil)
-    !restday?(date, calendar: calendar)
-  end
+    def self.workday?(date, calendar: :brasil)
+      !restday?(date, calendar: calendar)
+    end
 
-  def self.last_workday(date, calendar: :brasil)
-    return date if workday?(date, calendar: calendar)
+    def self.last_workday(date, calendar: :brasil)
+      return date if workday?(date, calendar: calendar)
 
-    last_workday((date - 1.day), calendar: calendar)
-  end
+      last_workday((date - 1.day), calendar: calendar)
+    end
 
-  def self.next_workday(date, calendar: :brasil)
-    return date if workday?(date, calendar: calendar)
+    def self.next_workday(date, calendar: :brasil)
+      return date if workday?(date, calendar: calendar)
 
-    next_workday((date + 1.day), calendar: calendar)
+      next_workday((date + 1.day), calendar: calendar)
+    end
   end
 end
--- spec/lib/period/workday_spec.rb
+++ spec/lib/period/workday_spec.rb
@@ -1,23 +1,23 @@
 require 'spec_helper'
 require 'period/workday'
 
-describe Workday do
+describe Period::Workday do
   describe '#workday?' do
     context 'monday' do
       it 'is a workday' do
-        expect(Workday.workday?(Date.new(2012, 8, 6))).to eq(true)
+        expect(described_class.workday?(Date.new(2012, 8, 6))).to eq(true)
       end
     end
 
     context 'saturday' do
       it 'is not a workday' do
-        expect(Workday.workday?(Date.new(2012, 8, 4))).to eq(false)
+        expect(described_class.workday?(Date.new(2012, 8, 4))).to eq(false)
       end
     end
 
     context 'sunday' do
       it 'is not a workday' do
-        expect(Workday.workday?(Date.new(2012, 8, 5))).to eq(false)
+        expect(described_class.workday?(Date.new(2012, 8, 5))).to eq(false)
       end
     end
 
@@ -25,7 +25,7 @@ describe Workday do
       it 'is not a workday' do
         ['2012-06-07', '2012-09-07', '2012-10-12',
          '2012-11-02', '2012-11-15', '2012-12-25'].each do |holiday|
-          expect(Workday.workday?(Date.parse(holiday))).to eq(false)
+          expect(described_class.workday?(Date.parse(holiday))).to eq(false)
         end
       end
     end
@@ -33,7 +33,7 @@ describe Workday do
     context 'holidays using bovespa calendar' do
       it 'is not a workday' do
         ['2012-07-09', '2012-11-20', '2012-12-24'].each do |holiday|
-          expect(Workday.workday?(Date.parse(holiday), calendar: :bovespa)).to eq(false)
+          expect(described_class.workday?(Date.parse(holiday), calendar: :bovespa)).to eq(false)
         end
       end
     end
@@ -44,19 +44,19 @@ describe Workday do
     context 'Using Date objects' do
       context 'monday' do
         it 'is not a restday' do
-          expect(Workday.restday?(Date.new(2012, 8, 6))).to eq(false)
+          expect(described_class.restday?(Date.new(2012, 8, 6))).to eq(false)
         end
       end
 
       context 'saturday' do
         it 'is a restday' do
-          expect(Workday.restday?(Date.new(2012, 8, 4))).to eq(true)
+          expect(described_class.restday?(Date.new(2012, 8, 4))).to eq(true)
         end
       end
 
       context 'sunday' do
         it 'is a restday' do
-          expect(Workday.restday?(Date.new(2012, 8, 5))).to eq(true)
+          expect(described_class.restday?(Date.new(2012, 8, 5))).to eq(true)
         end
       end
 
@@ -70,7 +70,7 @@ describe Workday do
           Date.new(2012, 12, 25)
         ].each do |holiday|
           it 'is a restday' do
-            expect(Workday.restday?(holiday)).to eq(true)
+            expect(described_class.restday?(holiday)).to eq(true)
           end
         end
       end
@@ -79,19 +79,19 @@ describe Workday do
     context 'using DateTime objects' do
       context 'monday' do
         it 'is not a restday' do
-          expect(Workday.restday?(DateTime.new(2012, 8, 6, 12))).to eq(false)
+          expect(described_class.restday?(DateTime.new(2012, 8, 6, 12))).to eq(false)
         end
       end
 
       context 'saturday' do
         it 'is a restday' do
-          expect(Workday.restday?(DateTime.new(2012, 8, 4, 12))).to eq(true)
+          expect(described_class.restday?(DateTime.new(2012, 8, 4, 12))).to eq(true)
         end
       end
 
       context 'sunday' do
         it 'is a restday' do
-          expect(Workday.restday?(DateTime.new(2012, 8, 5, 13))).to eq(true)
+          expect(described_class.restday?(DateTime.new(2012, 8, 5, 13))).to eq(true)
         end
       end
 
@@ -104,7 +104,7 @@ describe Workday do
           DateTime.new(2017, 12, 25, 04)
         ].each do |holiday|
           it 'is a restday' do
-            expect(Workday.restday?(holiday)).to eq(true)
+            expect(described_class.restday?(holiday)).to eq(true)
           end
         end
       end
@@ -114,26 +114,26 @@ describe Workday do
   describe '#last_workday' do
     context 'when date is 2012-10-25 (Thursday)' do
       it 'returns the same date' do
-        expect(Workday.last_workday(Date.new(2012, 10, 25))).to eq(Date.new(2012, 10, 25))
+        expect(described_class.last_workday(Date.new(2012, 10, 25))).to eq(Date.new(2012, 10, 25))
       end
     end
 
     context 'when date is 2012-10-21 (Sunday)' do
       it 'returns 2012-10-19 (Friday)' do
-        expect(Workday.last_workday(Date.new(2012, 10, 21))).to eq(Date.new(2012, 10, 19))
+        expect(described_class.last_workday(Date.new(2012, 10, 21))).to eq(Date.new(2012, 10, 19))
       end
     end
 
     context 'when date is 2012-10-20 (Saturday)' do
       it 'returns 2012-10-19 (Friday)' do
-        expect(Workday.last_workday(Date.new(2012, 10, 20))).to eq(Date.new(2012, 10, 19))
+        expect(described_class.last_workday(Date.new(2012, 10, 20))).to eq(Date.new(2012, 10, 19))
       end
     end
 
     context 'when bovespa calendar' do
       context 'when date is 2013-01-01 (bovespa holiday)' do
         it 'returns 2012-12-30,  because 2012-12-31 is also a bovespa holiday and 2012-01-30|29 is a weekend' do
-          expect(Workday.last_workday(Date.new(2013, 1, 1), calendar: :bovespa)).to eq(Date.new(2012, 12, 28))
+          expect(described_class.last_workday(Date.new(2013, 1, 1), calendar: :bovespa)).to eq(Date.new(2012, 12, 28))
         end
       end
     end
@@ -142,26 +142,26 @@ describe Workday do
   describe '#next_workday' do
     context 'when date is 2012-10-26 (Friday)' do
       it 'returns the same date' do
-        expect(Workday.next_workday(Date.new(2012, 10, 26))).to eq(Date.new(2012, 10, 26))
+        expect(described_class.next_workday(Date.new(2012, 10, 26))).to eq(Date.new(2012, 10, 26))
       end
     end
 
     context 'when date is 2012-10-21 (Sunday)' do
       it 'returns 2012-10-22 (Monday)' do
-        expect(Workday.next_workday(Date.new(2012, 10, 21))).to eq(Date.new(2012, 10, 22))
+        expect(described_class.next_workday(Date.new(2012, 10, 21))).to eq(Date.new(2012, 10, 22))
       end
     end
 
     context 'when date is 2012-10-20 (Saturday)' do
       it 'returns 2012-10-22 (Monday)' do
-        expect(Workday.next_workday(Date.new(2012, 10, 20))).to eq(Date.new(2012, 10, 22))
+        expect(described_class.next_workday(Date.new(2012, 10, 20))).to eq(Date.new(2012, 10, 22))
       end
     end
 
     context 'when bovespa calendar' do
       context 'when date is 2012-12-30 (sunday)' do
         it 'returns 2013-01-02, because 2012, 12, 31 and 2013-01-01 are bovespa holidays' do
-          expect(Workday.next_workday(Date.new(2012, 12, 30), calendar: :bovespa)).to eq(Date.new(2013, 1, 2))
+          expect(described_class.next_workday(Date.new(2012, 12, 30), calendar: :bovespa)).to eq(Date.new(2013, 1, 2))
         end
       end
     end
